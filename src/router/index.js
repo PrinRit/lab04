d748cf1fc81c5e8e151d179581c2bb7d3f3bd8e3
Prin Ritti-traiphop <prinr2481@gmail.com>
2021-07-22 16:43:43 +0700

    4.2 Add Network error to the system when clickk any link when close server it will show Error

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import EventRegister from '@/views/event/Register.vue';
 import EventEdit from '@/views/event/Edit.vue';
 import EventLayout from '@/views/event/Layout.vue'
 import NotFound from '@/views/NotFound.vue'
+import NetWorkError from '@/views/NetworkError.vue'
 const routes = [
   {
     path: '/',
@@ -54,6 +55,11 @@ const routes = [
     path: '/:catchAll(.*)',
     name: 'NotFound',
     component: NotFound
+  },
+  {
+    path:'/network-error',
+    name: 'NetworkError',
+    component: NetWorkError
   }
 ]
 

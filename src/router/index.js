cae2ec876836cb65aa650a825561cced5c3275ed
Prin Ritti-traiphop <prinr2481@gmail.com>
2021-07-22 16:02:13 +0700

    3.2 add layout to reduce the code

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import About from '../views/About.vue'
 import EventDetails from '@/views/event/Details.vue';
 import EventRegister from '@/views/event/Register.vue';
 import EventEdit from '@/views/event/Edit.vue';
+import EventLayout from '@/views/event/Layout.vue'
 const routes = [
   {
     path: '/',
@@ -18,23 +19,30 @@ const routes = [
   },
   {
     path: '/event/:id',
-    name: 'EventDetails',
-    component: EventDetails,
-    props: true
-  },
-  {
-    path: '/event/:id/register',
-    name: 'EventRegister',
-    props:true,
-    component: EventRegister
-  },
-  {
-    path: '/event/:id/edit',
-    name:'EventEdit',
-    props:true,
-    component: EventEdit
-  }
+    name: 'EventLayout',
+    props: true,
+    component: EventLayout,
 
+    children: [
+      {
+        path: '',
+        name: 'EventDetails',
+        component: EventDetails
+      },
+      {
+        path: 'register',
+        name: 'EventRegister',
+        props: true,
+        component: EventRegister
+      },
+      {
+        path: 'edit',
+        name:'EventEdit',
+        props: true,
+        component: EventEdit
+      }
+    ]
+  }
 ]
 
 const router = createRouter({
